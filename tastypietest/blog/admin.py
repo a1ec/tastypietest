91faf940f0bf1ab3a693ee7d33bd2ac96bab5da5
ham@soccer.com <ham@soccer.com>
2017-07-14 10:27:58 +1000

    Added Admin Comment registration

--- tastypietest/blog/admin.py
+++ tastypietest/blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from blog.models import Entry
+from blog.models import Entry, Comment
 # Register your models here.
 admin.site.register(Entry)
+admin.site.register(Comment)
